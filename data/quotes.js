9830be87041f5f4f866abfcd096ce5e0a51ca2a2
Unikoli <uniqueoli16@gmail.com>
2025-05-07 15:36:44 +0545

    more quotes added!

--- data/quotes.js
+++ data/quotes.js
@@ -1,11 +1,14 @@
 const quotes = {
     science: [
-      { text: "Science is a way of thinking.", author: "Carl Sagan" },
-      { text: "Physics is the universe’s operating system.", author: "Steven R. Garman" },
+      { text: "Science never solves a problem without creating ten more.", author: "George Bernard" },
+      { text: "Science knows no country because knowledge belongs to humanity, and is the torch that illuminates the world", author: "Louis Pasteur" },
+      { text: "Science is magic that works", author: "Kurt Vonnegut" },
+      { text: "Science is the process that takes us from confusion to understanding.", author: "Brian Greene" },
     ],
     life: [
       { text: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
       { text: "The purpose of our lives is to be happy.", author: "Dalai Lama" },
+      { text: "life is all about struggle.", author: "satya murti" },
     ],
   };
   
