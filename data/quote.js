f160d507aedd28cef2d0a2fc6b9c60fea34d2393
Unikoli <uniqueoli16@gmail.com>
2025-05-07 15:21:15 +0545

    quotes added!

---
+++ data/quote.js
@@ -0,0 +1,11 @@
+const quotes = {
+    science: [
+      { text: "Science is a way of thinking.", author: "Carl Sagan" },
+      { text: "Physics is the universe’s operating system.", author: "Steven R. Garman" },
+    ],
+    life: [
+      { text: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
+      { text: "The purpose of our lives is to be happy.", author: "Dalai Lama" },
+    ],
+  };
+  
